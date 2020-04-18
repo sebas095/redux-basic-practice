b5c8ae402f5b368c7be5bedb4b7c7ec1c402e523
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-04-18 00:57:09 -0500

    [ADD] selected team component added

---
+++ src/components/SelectedTeam.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Titulars from "./Titulars";
+import Substitutes from "./Substitutes";
+
+const SelectedTeam = () => (
+  <section>
+    <Titulars />
+    <Substitutes />
+  </section>
+);
+
+export default SelectedTeam;
