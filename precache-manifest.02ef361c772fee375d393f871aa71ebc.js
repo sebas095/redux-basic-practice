d22632e439f7c5dd6fdd7dc2e48adecf8e7e2735
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-04-18 00:59:22 -0500

    Updates

---
+++ precache-manifest.02ef361c772fee375d393f871aa71ebc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79032d9e634745d62f4f1cd442281637",
+    "url": "/redux-basic-practice/index.html"
+  },
+  {
+    "revision": "09c40f584641712316a0",
+    "url": "/redux-basic-practice/static/css/main.930e3176.chunk.css"
+  },
+  {
+    "revision": "2c00338ce8c734564d0e",
+    "url": "/redux-basic-practice/static/js/2.c24505ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-basic-practice/static/js/2.c24505ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c40f584641712316a0",
+    "url": "/redux-basic-practice/static/js/main.4d800312.chunk.js"
+  },
+  {
+    "revision": "b17b9d91ebec6240414e",
+    "url": "/redux-basic-practice/static/js/runtime-main.db11b783.js"
+  },
+  {
+    "revision": "aa6d54bfa22c49cf58b85efd6f82d010",
+    "url": "/redux-basic-practice/static/media/cancha.aa6d54bf.svg"
+  }
+]);
